02942beb186a7744c9980479117f5fd9f173da34
palimato-ship-it <palimato-ship-it@users.noreply.github.com>
2026-09-20 06:09:49 +0000

    Deploy admin web app 246c3b33849503d4ade637ec98c787ad47412fa5 palimato-ship-it/APK-Study-Tool@246c3b33849503d4ade637ec98c787ad47412fa5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1883010270" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "750562105" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
